d5f67bed5da4dcdd8bbedca0ef57c38ee9480c73
kimtuan1102 <kimtuan1102@gmail.com>
2021-03-15 08:32:46 +0700

    handle missing message field

--- src/telegram/telegram.controller.ts
+++ src/telegram/telegram.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Logger,
+  Post,
+} from '@nestjs/common';
 import { TelegramService } from './telegram.service';
 import {
   ApiBearerAuth,
@@ -11,6 +18,7 @@ import {
 @ApiTags('Telegram')
 @ApiBearerAuth()
 export class TelegramController {
+  protected logger = new Logger(TelegramController.name);
   constructor(private telegramService: TelegramService) {}
 
   @Post('sync-telegram-account')
@@ -18,11 +26,17 @@ export class TelegramController {
   @ApiOperation({ description: 'Sync telegram account' })
   @ApiOkResponse()
   async syncTelegramAccount(@Body() body) {
-    const accountId = body.message.text;
-    const telegram = body.message.chat.id;
-    return await this.telegramService.syncTelegramAccount({
-      accountId,
-      telegram,
-    });
+    if (body.message) {
+      const accountId = body.message.text;
+      const telegram = body.message.chat.id;
+      return await this.telegramService.syncTelegramAccount({
+        accountId,
+        telegram,
+      });
+    }
+    this.logger.log('Field message does not exist');
+    return {
+      ok: true,
+    };
   }
 }
